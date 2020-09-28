6e493fcd554beffeff5ee2513443ff998f853865
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-09-27 18:56:51 -0600

    add questions

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-// array of questions for user
+const inquirer = require("inquirer")
+const axios = require("axios");
+const fs = require('fs');
+const path = require('path');
 const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+        {
+            type: "input",
+            message: "What is your Project Title?",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Provide with a detailed description.",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "Provide a guided Table of Contents.",
+            name: "Contents"
+        },
+        {
+            type: "input",
+            message: "Instalation, What steps are needed to install the project?",
+            name: "instruction"
+        },
+        {
+            type: "input",
+            message: "Provide instructions and examples for use.",
+            name: "Usage"
+        },
+        {
+            type: "input",
+            message: "List the collaborators on the project.",
+            name: "Collaborators"
+        },
+        {
+            type: "input",
+            message: "What License was used? Provide license ",
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "Will you allow others to contribute to your project? If so provide guidelines and restrictions.",
+            name: "contributing"
+        },
+        {
+            type: "input",
+            message: "Provide examples on how to run tests.",
+            name: "tests"
+        }
+        ];
